99c9afa50334ecd3f69478f50c7884cc75dd12ff
lars.ponikvar23@my.stjohns.edu <lars.ponikvar@gmail.com>
2026-09-02 13:47:34 -0400

    removed redundant comments

--- src/visu.py
+++ src/visu.py
@@ -1,5 +1,4 @@
 import numpy as np, pyvista as pv, random
-from pyvista import outline_algorithm
 
 def render(grid):
 
@@ -12,10 +11,3 @@ def render(grid):
         plotter.add_mesh(cube, color=random_color,show_edges=True, edge_color='black', line_width=1)
 
     plotter.show()
-
-# if __name__ == "__main__":
-#     grid = np.zeros((8,8,8),dtype=np.int8)
-#     grid[4,4,0]=1
-#     grid[4,4,1]=1
-#     grid[4,4,2]=1
-#     render(grid)
