7ccdc76092741bcda309e26a84d109c7178f0d47
lars.ponikvar23@my.stjohns.edu <lars.ponikvar@gmail.com>
2026-09-24 15:20:40 -0400

    novelty search now in evo loop

--- src/evolution.py
+++ src/evolution.py
@@ -1,5 +1,6 @@
 from env import Environment
 import numpy as np, torch, random, copy
+from fitness import bc, novelty
 
 class Archive:
     def __init__(self, rho_min=0.1, max_add=4, patience=5, raise_by=1.05, lower_by=0.95):
@@ -10,6 +11,7 @@ class Archive:
         self.lower_by = lower_by
         self.vectors = np.empty((0,5))
         self.gens_since_add =0
+
 
     def update(self, bcs,novelties):
 
@@ -30,7 +32,7 @@ class Archive:
             self.gens_since_add =0
 
 class Evolution:
-    def __init__(self, population_size, grid_shape, score_func, seed, agent_class): # creates population of agents all starting with random weights
+    def __init__(self, population_size, grid_shape, score_func, seed, agent_class, selection): # creates population of agents all starting with random weights
         torch.manual_seed(seed)
         random.seed(seed)
         self.best_grid=None
@@ -45,6 +47,8 @@ class Evolution:
         self.best_so_far=[]
         self.best =-1
         self.gen_mean=[]
+        self.selection = selection
+        self.archive = Archive()
 
     def run(self, steps=20, sigma=0.1, generations=100): # runs one full generation: each agent builds a structure and gets scored
         for generation in range(generations):
@@ -59,8 +63,19 @@ class Evolution:
                     x,y,z=np.unravel_index(index, self.grid_shape) # flat index back to 3D coords
                     if not env.place_block((x,y,z)):
                         break
-                self.scores[i]=self.score_func(env.grid)
-                self.grids[i]=env.grid
+
+                self.scores[i] = self.score_func(env.grid)
+                self.grids[i] = env.grid
+
+            if self.selection == "novelty": # checks if scores to rank on should be novelty or based on distance like objective
+                rows=[]
+                for g in self.grids:
+                    rows.append(bc(g))
+                bcs = np.array(rows)
+                rank_scores = novelty(bcs, self.archive.vectors)
+                self.archive.update(bcs, rank_scores)
+            else:
+                rank_scores = self.scores
 
             maximum=max(self.scores)
             self.gen_best.append(maximum)
@@ -71,7 +86,7 @@ class Evolution:
 
             self.gen_mean.append((sum(self.scores)/len(self.population)))
 
-            ranked=sorted(range(len(self.scores)), key=lambda i: self.scores[i], reverse=True) # ranks scores assigned to index
+            ranked=sorted(range(len(self.scores)), key=lambda i: rank_scores[i], reverse=True) # ranks scores assigned to index
             keep=len(self.population)//2 # drops lower half of scores
             survivors=[self.population[i] for i in ranked[:keep]]
 
